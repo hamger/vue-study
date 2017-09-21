3936e65dd4cdc224548c529499ea231c62be2b51
Hanger <2313303800@qq.com>
2017-09-21 19:52:57 +0800

    Update util.js

--- vue-es6/src/vue-mine2/util.js
+++ vue-es6/src/vue-mine2/util.js
@@ -1,12 +1,22 @@
+/*
+Function : 为 obj.key 赋值并添加属性描述
+*/
 export function def(obj, key, value, enumerable) {
     Object.defineProperty(obj, key, {
         value: value,
         writeable: true,
         configurable: true,
         enumerable: !!enumerable
-    });
+    })
 }
 
+/*
+Function : 函数去抖
+Return : Function
+Explain : @func 需要去抖函数
+  @wait 去抖的时间长度
+  @immediate 是否立即执行
+*/
 export function debounce(func, wait, immediate) {
   var timeout = null;
   
