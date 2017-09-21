7f99e9cb342831b69a7180616a1a222934ac9d66
Hanger <2313303800@qq.com>
2017-09-21 17:05:18 +0800

    Update array.js

--- vue-es6/src/vue-mine2/array.js
+++ vue-es6/src/vue-mine2/array.js
@@ -3,6 +3,7 @@ import { def } from './util';
 const arrayProto = Array.prototype;
 export const arrayMethods = Object.create(arrayProto);
 
+// 使用 arrayMethods 代理原始的数组方法，使其可遍历
 [
   'push',
   'pop',
@@ -12,15 +13,17 @@ export const arrayMethods = Object.create(arrayProto);
   'sort',
   'reverse'
 ].forEach(function(method) {
-    const original = arrayProto[method];
+    const original = arrayProto[method]; // 数组方法
     def(arrayMethods, method, function() {
+        // 创建一个数组，代理arguments
         let i = arguments.length;
         const args = new Array(i);
         while(i--) {
             args[i] = arguments[i];
         }
-        const result = original.apply(this, args);
-        const ob = this.__ob__;
+
+        const result = original.apply(this, args); // 挂载数组方法
+        const ob = this.__ob__; // ob是每个数组对象对应的obeserver对象
         let inserted;
         switch(method) {
             case 'push':
@@ -33,9 +36,11 @@ export const arrayMethods = Object.create(arrayProto);
                 inserted = args.slice(2);
                 break;
         }
-        if (inserted) ob.observerArray(inserted);
 
+        // 如果监听的data中的对象有push,unshift,splice等添加新值的方法，就监听新值
+        if (inserted) ob.observerArray(inserted);
+        // 每次使用数组方法，都触发数组对象的dep发布事件
         ob.dep.notify();
-        return result;
+        return result; // 返回数组处理方法
     });
 });
