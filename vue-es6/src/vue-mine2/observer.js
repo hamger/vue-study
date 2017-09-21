aed2a894636250cf85297b35b0cf9ab64cfa57f8
Hanger <2313303800@qq.com>
2017-09-21 19:52:04 +0800

    Update observer.js

--- vue-es6/src/vue-mine2/observer.js
+++ vue-es6/src/vue-mine2/observer.js
@@ -1,18 +1,19 @@
 import Dep from './dep';
 import { def } from './util';
+// arrayMethods是代理了数组方法的对象
 import { arrayMethods } from './array';
 
-
+// 原型继承
 function protoAugment(target, src) {
     target.__proto__ = src;
 }
-
+// 赋值继承
 function copyAugment(target, src, keys) {
     for (let i = 0; i < keys.length; i++) {
-        def(target, key[i], src[key[i]]);
+        def(target, keys[i], src[keys[i]]);
     }
 }
-
+// obeserver只监听对象。
 export default function observer(data) {
     if (!data || typeof data !== 'object') {
         return;
@@ -25,19 +26,22 @@ export default function observer(data) {
 class Observer {
     constructor(data) {
         this.dep = new Dep();
+        // 给每个数据一个指向Observer的引用，array.js会用到
         def(data, "__ob__", this);
         this.data = data;
-
+        // vm.data中如果存在数组，会在后面的递归中进入下面if这个流程
         if (Array.isArray(data)) {
             const argment = data.__proto__ ? protoAugment : copyAugment;
+            // 开始覆盖data数组的原生方法
             argment(data, arrayMethods, Object.keys(arrayMethods));
+            // 对数组元素遍历下，有元素可能是对象
             this.observerArray(data);
         } else {
             this.walk(data);
         }
-
     }
 
+    // 遍历对象的所有属性
     walk(data) {
         let self = this;
         Object.keys(this.data).forEach(function (key) {
@@ -45,28 +49,34 @@ class Observer {
         });
     }
 
+    // 观察数组
     observerArray(items) {
         for (let i = 0; i < items.length; i++) {
-            observer(items[i]);
+            observer(items[i]); // 数组的元素是对象就监听
         }
     }
 
+    /**
+     * Define a reactive property on an Object.（952~1006）
+     */
     defineReactice(data, key, value) {
+        // 每个属性都有一个dep
         let dep = new Dep(),
             descriptor = Object.getOwnPropertyDescriptor(data, key);
+        // 如果已经存在访问器且访问器接口的configurable属性为false则直接返回
         if (descriptor && !descriptor.configurable) {
             return;
         }
-
+        //递归监听，value是对象会返回一个new Observer(value)，否则childObserver为undifined
         let childObserver = observer(value);
-
         Object.defineProperty(data, key, {
             enumerable: true,
             configurable: false,
             get: function () {
+                //get函数会在node编译时初始化赋值时触发，此时Dep.target会指向那个watcher观察者
                 if (Dep.target) {
-                    dep.depend();
-                    if (childObserver) {
+                    dep.depend(); // 添加 dep，没一个属性都会有一个dep对象
+                    if (childObserver) { // 为子数据添加dep
                         childObserver.dep.depend();
                     }
                 }
@@ -74,15 +84,14 @@ class Observer {
             },
             set: function (newValue) {
                 if (newValue == value) {
-                    return;
+                    return; // 数据没有变化直接返回
                 }
                 if (typeof newValue === 'object') {
-                    observer(newValue);
+                    observer(newValue); // 如果新值是对象观察之
                 }
                 value = newValue;
-                dep.notify();
+                dep.notify(); // 发布更新公告
             }
         });
     }
 }
-
