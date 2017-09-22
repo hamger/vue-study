8521ed8f3c3a3ee81393bd617dd148ed7c6162be
Hanger <2313303800@qq.com>
2017-09-22 09:29:49 +0800

    Update observer.js

--- vue-es6/src/vue-mine/observer.js
+++ vue-es6/src/vue-mine/observer.js
@@ -1,28 +1,32 @@
 import Dep from './dep'
 
+// 遍历 vm.data 的所有属性
 export default function observer (obj,vm) {
-	Object.keys(obj).forEach(key => { // 遍历对象属性
+	Object.keys(obj).forEach(key => { 
 		defineReactive(vm,key,obj[key])
 	})
 }
 
-// 定义响应式数据，将 data 放到 vm 下
+// 使用 Object.defineProperty 把这些属性全部转为 getter/setter
 function defineReactive (obj,key,val) { 
-	// 定义一个主题实例
+	// 实例化一个主题，每个属性都有一个 dep
 	var dep = new Dep()
 	Object.defineProperty(obj,key,{
 		get:function () {
+			// 初始化时添加订阅者
 			if(Dep.target){
-				dep.addSub(Dep.target) // 添加这个属性为订阅者
+				dep.addSub(Dep.target) 
 			}
 			return val
 		},
 		set:function (newVal) {
 			if (val === newVal) {
+				// 数据无变化直接返回
 				return
 			}
 			val = newVal
-			dep.notify() // 发布更新公告
+			// 发布更新公告
+			dep.notify() 
 		}
 	})
 }
