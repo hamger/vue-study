04e4a665503158ea368fd537920855a321a82f3e
Hanger <2313303800@qq.com>
2017-09-22 09:28:17 +0800

    Update compiler.js

--- vue-es6/src/vue-mine/compiler.js
+++ vue-es6/src/vue-mine/compiler.js
@@ -1,22 +1,26 @@
 import Watcher from './watcher'
 
 export default class Compiler {
-    constructor(node, vm) {
-    	// 创建文档碎片节点
+    constructor(el, vm) {
+        this.$el = document.querySelector(el)
+    	// 创建文档片段
     	let flag = document.createDocumentFragment()
     	let child 
-    	while(child = node.firstChild){ // 当 node 下无子节点时停止编译
+        // 编译每个节点，直到 this.$el 下无子节点
+    	while(child = this.$el.firstChild){ 
     		this.compile(child,vm)
-    		flag.appendChild(child)
+            // appendchild 方法会自动删除 node 对象的 child 节点
+    		flag.appendChild(child) 
     	}
-    	node.appendChild(flag)
+    	this.$el.appendChild(flag)
     }
 
     // 编译节点
     compile(node, vm) { 
         const reg = /\{\{(.*)\}\}/
-        const type = node.nodeType
-        if (type === 1) { // 元素节点
+        const type = node.nodeType 
+        // 该节点为元素节点
+        if (type === 1) { 
             const attr = node.attributes
             for (let i = 0; i < attr.length; i++) {
                 if (attr[i].nodeName == 'v-model') {
@@ -28,10 +32,12 @@ export default class Compiler {
                 }
             };
         }
-        if (type === 3) { // 文本节点
+        // 该节点为文本节点
+        if (type === 3) {
             if (reg.test(node.nodeValue)) {
                 const key = RegExp.$1.trim()
-                new Watcher(vm, node, key)
+                // 初始化数据并添加订阅者
+                new Watcher(vm, node, key) 
             }
         }
     }
