c185deb4b5aac75a34d81e8192b601706f61bd1e
Hanger <2313303800@qq.com>
2017-09-22 09:29:00 +0800

    Update MVVM.js

--- vue-es6/src/vue-mine/MVVM.js
+++ vue-es6/src/vue-mine/MVVM.js
@@ -4,13 +4,11 @@ import Compiler from './compiler'
 class MVVM {
 	constructor (options) {
 		this.data = options.data
-		const id = options.el
-
 		// 监听数据
 		observer(this.data,this)
 
 		// 编译节点,更新view
-		new Compiler(document.getElementById(id),this)
+		new Compiler(options.el || document.body,this)
 	}
 }
 
