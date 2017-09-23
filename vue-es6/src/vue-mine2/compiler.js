a7500d230a98431fa7d178409328c7c419a93446
Hanger <2313303800@qq.com>
2017-09-23 21:03:43 +0800

    Update compiler.js

--- vue-es6/src/vue-mine2/compiler.js
+++ vue-es6/src/vue-mine2/compiler.js
@@ -1,159 +1,216 @@
-import Watcher from './watcher';
-import observer from './observer';
+import Watcher from './watcher'
+import observer from './observer'
 
+// 定义解析含标识符文本的正则
 const tagRE = /\{\{\{(.*?)\}\}\}|\{\{(.*?)\}\}/g,
     htmlRE = /^\{\{\{(.*)\}\}\}$/,
     paramsRE = /\((.+)\)/g,
-    stringRE = /\'(.*)\'/g;
+    stringRE = /\'(.*)\'/g
 
 
 export default class Compiler {
 
     constructor(el, vm) {
-        this.$vm = vm;
-        this.$el = this.isElementNode(el) ? el : document.querySelector(el);
+        this.$vm = vm
+        this.$el = this.isElementNode(el) ? el : document.querySelector(el)
 
         if (this.$el) {
-            this.$fragment = this.createFragment(this.$el);
-            this.compileElement(this.$fragment);
-            this.$el.appendChild(this.$fragment);
+            // 将 DOM 转化为文档片段
+            this.$fragment = this.createFragment(this.$el)
+            // 编译文档片段，编译器的核心入口
+            this.compileElement(this.$fragment)
+            // 将编译后的文档片段添加到目标 DOM 中
+            this.$el.appendChild(this.$fragment)
         }
     }
 
+    /*
+    创建文档片段
+    */
     createFragment(el) {
         var fragment = document.createDocumentFragment(),
-            child;
-
+            child
+        // 遍历原始node子节点，appendchild 方法会自动删除 node 对象的 child 节点
+        // （一个字节点有且仅有一个父节点）
+        // 这里只是遍历子节点，在 compileNodeAttr 函数中会递归遍历后代节点
         while (child = el.firstChild) {
-            fragment.appendChild(child);
+            fragment.appendChild(child)
         }
 
-        return fragment;
+        return fragment
     }
 
+    /*
+    编译文档片段
+    */
     compileElement(el) {
         let childNodes = el.childNodes,
-            self = this;
-
+            self = this
+        // 将子元素转化为数组，遍历之
         [].slice.call(childNodes).forEach(function (node) {
-            var text = node.textContent;
-            var reg = /\{\{(.*)\}\}/g;
-
-            if (self.isElementNode(node)) {
-                self.compileNodeAttr(node);
+            var text = node.textContent
+            var reg = /\{\{(.*)\}\}/g
+            // 如果 node 类型为元素
+            if (self.isElementNode(node)) { 
+                self.compileNodeAttr(node)  
+            // 如果 node 类型为文本，且存在 {{ }} 文本标识符
             } else if (self.isTextNode(node) && reg.test(text)) {
-                self.compileText(node);
+                self.compileText(node)
             }
-        });
+        })
     }
 
+    /*
+    编译节点属性
+    */
     compileNodeAttr(node) {
+        // 获得元素节点的所有属性
         let nodeAttrs = node.attributes,
             self = this,
             lazyComplier,
-            lazyExp;
+            lazyExp
 
+        // 将所有属性转化为数组，遍历之
         [].slice.call(nodeAttrs).forEach(function (attr) {
-            let attrName = attr.name;
+            let attrName = attr.name
+            // 是否存在合法指令
             if (self.isDirective(attrName)) {
-                let expression = attr.value;
-                // directicve
-                let directive = attrName.substring(2);
+                let expression = attr.value
+                // 得到指令（去除"v-"的部分）
+                let directive = attrName.substring(2)
+                // 如果指令是"for"(for循环)
                 if (directive === 'for') {
-                    lazyComplier = directive;
-                    lazyExp = expression;
+                    // 储存指令
+                    lazyComplier = directive
+                    // 储存表达式
+                    lazyExp = expression
+                // 如果指令是事件指令
                 } else if (self.isEventDirective(directive)) {
-                    directiveUtil.addEvent(node, self.$vm, directive, expression);
+                    // 添加该事件
+                    directiveUtil.addEvent(node, self.$vm, directive, expression)
+                // 其他指令
                 } else {
-                    directiveUtil[directive] && directiveUtil[directive](node, self.$vm, expression);
+                    directiveUtil[directive] && directiveUtil[directive](node, self.$vm, expression)
                 }
-                node.removeAttribute(attrName);
+                // 用完指令后移除，防止渲染在 DOM 上
+                node.removeAttribute(attrName)
             }
         });
 
         if (lazyComplier === 'for') {
-            directiveUtil[lazyComplier] && directiveUtil[lazyComplier](node, this.$vm, lazyExp);
+            // 执行 for 循环
+            directiveUtil[lazyComplier] && directiveUtil[lazyComplier](node, this.$vm, lazyExp)
         } else if (node.childNodes && node.childNodes.length) {
-            self.compileElement(node);
+            // 递归编译文档片段，直到没有元素节点
+            self.compileElement(node)
         }
     }
 
+    /*
+    编译文本节点
+    */
     compileText(node) {
-        const tokens = this.parseText(node.wholeText);
-        let fragment = document.createDocumentFragment();
+        // 获得文本解析后的一个数组
+        const tokens = this.parseText(node.wholeText)
+        let fragment = document.createDocumentFragment()
         tokens.forEach(token => {
-            let el;
+            let el
+            // 需要编译的文本
             if (token.tag) {
+                // HTML 文本
                 if (token.html) {
-                    el = document.createDocumentFragment();
-                    el.$parent = node.parentNode;
-                    el.$oncetime = true;
-                    directiveUtil.html(el, this.$vm, token.value);
-
+                    el = document.createDocumentFragment()
+                    el.$parent = node.parentNode
+                    el.$oncetime = true
+                    // 进行监听并赋值
+                    directiveUtil.html(el, this.$vm, token.value)
+                // 普通文本
                 } else {
-                    el = document.createTextNode(" ");
-                    directiveUtil.text(el, this.$vm, token.value);
+                    el = document.createTextNode(" ")
+                    // 进行监听并赋值
+                    directiveUtil.text(el, this.$vm, token.value)
                 }
+            // 不需要编译的文本
             } else {
-                el = document.createTextNode(token.value);
+                el = document.createTextNode(token.value)
             }
-            el && fragment.appendChild(el);
-        });
-        node.parentNode.replaceChild(fragment, node);
+            el && fragment.appendChild(el)
+        })
+        // 更新节点的子节点
+        node.parentNode.replaceChild(fragment, node)
     }
 
+    /*
+    解析文本内容，返回一个数组
+    */
     parseText(text) {
-
         if (!tagRE.test(text)) {
-            return;
+            return
         }
-        const tokens = [];
-        let lastIndex = tagRE.lastIndex = 0;
-        let match, index, html, value;
+        const tokens = []
+        let lastIndex = tagRE.lastIndex = 0
+        let match, index, html, value
         while (match = tagRE.exec(text)) {
-            index = match.index;
-            // 先把{{}} 或者 {{{}}} 之前的文本提取
+            index = match.index
+            // 获取 {{}} 或者 {{{}}} 之前的文本
             if (index > lastIndex) {
                 tokens.push({
                     value: text.slice(lastIndex, index)
                 });
             }
-            html = htmlRE.test(match[0]);
-            value = html ? match[1] : match[2];
+
+            // 获取 {{}} 或者 {{{}}} 中的文本
+            html = htmlRE.test(match[0])
+            // 如果是 {{{}}} 则为 HTML 文本 ，否则为普通文本
+            value = html ? match[1] : match[2]
             tokens.push({
                 value: value,
+                // 标识是否为需要编译的文本
                 tag: true,
-                html: html
+                // 标识是否为 HTML 文本
+                html: html 
             });
-            lastIndex = index + match[0].length;
+            lastIndex = index + match[0].length
         }
-
+        // 获取 {{}} 或者 {{{}}} 之后的文本
         if (lastIndex < text.length) {
             tokens.push({
                 value: text.slice(lastIndex)
-            });
+            })
         }
-        return tokens;
+        return tokens 
     }
 
+    /*
+    是否存在 v 指令
+    */
     isDirective(attr) {
-        return attr.indexOf('v-') === 0;
+        return attr.indexOf('v-') === 0
     }
 
+    /*
+    是否存在 on 指令
+    */
     isEventDirective(dir) {
-        return dir.indexOf('on') === 0;
+        return dir.indexOf('on') === 0
     }
 
+    /*
+    是否是元素节点
+    */
     isElementNode(node) {
-        return node.nodeType === 1;
+        return node.nodeType === 1
     }
-
+    /*
+    是否是文本节点
+    */
     isTextNode(node) {
-        return node.nodeType === 3;
+        return node.nodeType === 3
     }
 }
 
 
+// 指令工具函数集合
 const directiveUtil = {
     text: function (node, vm, expression) {
         this.bind(node, vm, expression, 'text');
@@ -205,7 +262,6 @@ const directiveUtil = {
                 new Compiler(cloneNode, forVm);
             });
         });
-
     },
 
     model: function (node, vm, expression) {
@@ -237,6 +293,7 @@ const directiveUtil = {
         var updaterFn = updater[directive + 'Updater'];
         let value = this._getVMVal(vm, expression);
         updaterFn && updaterFn(node, value);
+        // 监听数据并更新数据
         new Watcher(vm, expression, function (newValue, oldValue) {
             updaterFn && updaterFn(node, newValue, oldValue);
         });
@@ -272,45 +329,57 @@ const directiveUtil = {
         }
     },
 
+    /*
+    获取 vm 上的数据
+    */
     _getVMVal: function (vm, expression) {
-        expression = expression.trim();
-        let value = vm;
-        expression = expression.split('.');
+        expression = expression.trim()
+        let value = vm
+        // 处理 data.data2.data3 类似的多层对象
+        expression = expression.split('.')
         expression.forEach((key) => {
             if (value.hasOwnProperty(key)) {
-                value = value[key];
+                // 下次遍历的时候 value 的值也将改变
+                // 正好可以用来获取多层的对象属性值
+                value = value[key]; 
             } else {
-                throw new Error("can not find the property: " + key);
+                throw new Error("can not find the property: " + key)
             }
-
         });
 
         if (typeof value === 'object') {
-            return JSON.stringify(value);
+            return JSON.stringify(value)
         } else {
-            return value;
+            return value
         }
     },
 
+    /*
+    变更 vm 上的数据
+    */
     _setVMVal: function (vm, expression, value) {
         expression = expression.trim();
         let data = vm._data;
         expression = expression.split('.');
         expression.forEach((key, index) => {
+            // 当遍历到最后一个属性的时候进行赋值
             if (index == expression.length - 1) {
-                data[key] = value;
+                data[key] = value
+            // 如果不是最后一个属性则改变 data
             } else {
-                data = data[key];
+                data = data[key]
             }
         });
     }
 }
 
-const cacheDiv = document.createElement('div');
+// 创建容器元素
+const cacheDiv = document.createElement('div')
 
+// 更新方法集合
 const updater = {
     textUpdater: function (node, value) {
-        node.textContent = typeof value === 'undefined' ? '' : value;
+        node.textContent = typeof value === 'undefined' ? '' : value
     },
 
     htmlUpdater: function (node, value) {
