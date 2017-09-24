2adb3c08265ed2bf85cabd5b33aaffd9721c0089
Hanger <2313303800@qq.com>
2017-09-24 10:50:57 +0800

    Update watcher.js

--- vue-es6/src/vue-mine2/watcher.js
+++ vue-es6/src/vue-mine2/watcher.js
@@ -2,57 +2,55 @@ import Dep from './dep'
 
 export default class Watcher {
     constructor(vm, expression, callback) {
-        this.vm = vm;
-        // 目标数据属性的表达式
-        this.expression = expression;
-        this.callback = callback;
+        this.vm = vm
+        // 目标数据属性的表达式（key1.key2...）
+        this.expression = expression
+        this.callback = callback
         // watcher 监听的属性的Id
-        this.depIds = {};
+        this.depIds = {}
         // 备份，以便判断变化
-        this.oldValue = this.get();
+        this.oldValue = this.get()
     }
 
     // 更新数据并执行回调
     update () {
-        let newValue = this.get();
-        let oldValue = this.oldValue;
+        let newValue = this.get()
+        let oldValue = this.oldValue
         if (newValue !== this.oldValue) {
             // 更新老数据备份
-            this.oldValue = newValue; 
+            this.oldValue = newValue
             // 执行回调更新视图
-            this.callback.call(this.vm, newValue, oldValue);
+            this.callback.call(this.vm, newValue, oldValue)
         }
     }
 
-    // 添加主题
     addDep (dep) {
         if (!this.depIds.hasOwnProperty(dep.id)) {
-            // 该主题中添加订阅者自身
-            dep.addSub(this);
-            // 该属性的依赖列表
-            this.depIds[dep.id] = dep;
+            dep.addSub(this) // 添加订阅者
+            this.depIds[dep.id] = dep // 增加依赖项
         }
     }
 
-    // 取得 node 的 expresstion 在 vm 中的值
+    // 取得 node 的 expresstion 在 vm 中的值   
     get () {
-        Dep.target = this;
-        // 求值的过程会触发vm属性值的getter
-        let value = this.getVMVal();
-        // 访问完了，置空
-        Dep.target = null;
-        return value;
+        // 静态属性，指向 Watcher 实例
+        Dep.target = this
+        // 求值的过程会触发vm属性值的 getter
+        let value = this.getVMVal()
+        // 置空，防止重复添加订阅者
+        Dep.target = null
+        return value
     }
 
     getVMVal () {
-        let expression = this.expression.split('.');
-        let value = this.vm;
+        let expression = this.expression.split('.')
+        let value = this.vm
         expression.forEach(function (curVal) {
             // 这里取值的过程，会调用到每一个数据的get，根据getter里面的闭包
             // 从而访问到数据的dep,调用dep.depend
             // 属性dep.depend, 进一步调用到Watch的addDep，让watcher添加进去
-            value = value[curVal];
-        });
-        return value;
+            value = value[curVal]
+        })
+        return value
     }
 }
