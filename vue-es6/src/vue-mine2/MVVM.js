b36a779d7c9df4dc5ceb55568de693c40f080436
Hanger <2313303800@qq.com>
2017-09-24 10:43:38 +0800

    Update MVVM.js

--- vue-es6/src/vue-mine2/MVVM.js
+++ vue-es6/src/vue-mine2/MVVM.js
@@ -5,38 +5,38 @@ import Compiler from './compiler'
 class MVVM {
 
     constructor (options) {
-        this.$options = options;
-        this._data = this.$options.data;
-        var self = this;
+        this.$options = options
+        this._data = this.$options.data
+        var self = this
         // 遍历 vm.data，并使 vm 代理 vm.data
         Object.keys(this.$options.data).forEach(key => {
-            this._proxy(key);
+            this._proxy(key)
         });
         // 观察 vm
-        observer(this._data);
+        observer(this._data)
 
         // 编译节点，解析各种指令，并且将每个 node 节点对应一个 watcher 身份，在收到通知时改变自身视图
-        this.$compiler = new Compiler(options.el || document.body, this);
+        this.$compiler = new Compiler(options.el || document.body, this)
     }
 
     $watch (expression, callback) {
-        new Watcher(this, expression, callback);
+        new Watcher(this, expression, callback)
     }
 
     // 代理数据
     _proxy (key) {
-        let self = this;
+        let self = this
         Object.defineProperty(this, key, {
             configurable: false,
             enumerable: true,
             get() {
-                return self._data[key];
+                return self._data[key]
             },
             set(value) {
-                self._data[key] = value;
+                self._data[key] = value
             }
-        });
+        })
     }
 }
 
-window.MVVM = MVVM;
+window.MVVM = MVVM
