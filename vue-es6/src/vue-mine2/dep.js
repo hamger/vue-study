799cedaa89e432b6b9499418b70c8ecef6f874af
Hanger <2313303800@qq.com>
2017-09-24 10:48:59 +0800

    Update dep.js

--- vue-es6/src/vue-mine2/dep.js
+++ vue-es6/src/vue-mine2/dep.js
@@ -1,29 +1,29 @@
-let uid = 0;
+let uid = 0 // 主题 ID
 
 export default class Dep {
-    static target;
+    static target // 静态属性，用来存放订阅者
 
     constructor () {
-        this.id = uid++;
-        this.subs = [];
+        this.id = uid++
+        this.subs = [] // 订阅者集合，订阅者是 Watcher 的实例
     }
 
     addSub (sub) { // 添加订阅者
-        this.subs.push(sub); 
+        this.subs.push(sub)
     }
 
     removeSub (sub) { // 取消订阅
-        let index = this.subs.indexOf(sub);
+        let index = this.subs.indexOf(sub)
         if (index != -1) {
-            this.subs.splice(index, 1);
+            this.subs.splice(index, 1)
         }
     }
 
-    depend () { // 添加自身
-        Dep.target.addDep(this); 
+    depend () { // 添加订阅者和依赖项
+        Dep.target.addDep(this)
     }
 
-    notify () { // 主题发布更新公告
-        this.subs.forEach(sub => sub.update());
+    notify () { // 发布更新公告
+        this.subs.forEach(sub => sub.update())
     }
 }
